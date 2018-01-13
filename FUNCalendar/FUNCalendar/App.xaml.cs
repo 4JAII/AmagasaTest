1e998665858e9bec2cec7f051e66c52138ca1a0e
Amagasa Tomoya <Thomsoon2017@gmail.com>
2018-01-13 22:07:56 +0900

    転送

---
+++ FUNCalendar/FUNCalendar/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Unity;
+using FUNCalendar.Views;
+using FUNCalendar.Models;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Microsoft.Practices.Unity;
+
+namespace FUNCalendar
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("/RootPage/NavigationPage/HouseHoldAccountsStatisticsPage");
+        }
+
+        protected override void RegisterTypes()
+        {
+            /* 画面をDIコンテナに登録 */
+            Container.RegisterTypeForNavigation<RootPage>();
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MenuPage>();
+            Container.RegisterTypeForNavigation<CalendarPage>();
+            Container.RegisterTypeForNavigation<WishListPage>();
+            Container.RegisterTypeForNavigation<ToDoListPage>();
+            Container.RegisterTypeForNavigation<WishListRegisterPage>();
+            Container.RegisterTypeForNavigation<HouseHoldAccountsStatisticsPage>();
+            Container.RegisterTypeForNavigation<HouseHoldAccountsSCStatisticsPage>();
+
+            /* 共有のインスタンスをDIコンテナに登録 */
+            Container.RegisterType<IWishList,WishList>(new ContainerControlledLifetimeManager());
+            Container.RegisterType<IHouseHoldAccounts, HouseHoldAccounts>(new ContainerControlledLifetimeManager());
+            Container.RegisterTypeForNavigation<HouseholdaccountsDCHistoryPage>();
+            Container.RegisterTypeForNavigation<HouseholdaccountBalancePage>();
+            Container.RegisterTypeForNavigation<HouseholdaccountsHistoryPage>();
+            Container.RegisterTypeForNavigation<HouseholdaccountsRegisterPage>();
+        }
+    }
+}
---
+++ FUNCalendar/FUNCalendar/ViewModels/Householdaccounts/Item/VMHouseHoldAccountsItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FUNCalendar.Models;
+
+namespace FUNCalendar.ViewModels
+{
+    public class VMHouseHoldAccountsItem
+    {
+        public string Name { get; private set; }
+        public string Price { get; private set; }
+        public string Date { get; private set; }
+        public string SCategory { get; private set; }
+        public string DCategory { get; private set; }
+        public string Storagetype { get; private set; }
+        public string CategoryData { get; private set; }
+        public string IsOutGoings { get; private set; }
+
+        /* コンストラクタ(HouseHoldAccountsItem To VMHouseHoldAccountsItem) */
+        public VMHouseHoldAccountsItem(HouseHoldAccountsItem item)
+        {
+            this.Name = item.Name;
+            this.Price = string.Format("{0}円", item.Price);
+            this.Date = item.Date.ToString("yyyy/MM/dd");
+            this.SCategory = Enum.GetName(typeof(SCategorys), item.SCategory);
+            this.DCategory = Enum.GetName(typeof(DCategorys), item.DCategory);
+            this.Storagetype = Enum.GetName(typeof(StorageTypes), item.StorageType);
+            this.IsOutGoings = item.IsOutGoings ? "支出" : "収入";
+            this.CategoryData = string.Format("{0}>{1}>{2}",IsOutGoings, item.SCategory, item.DCategory);
+        }
+
+        /* 編集用 */
+        public VMHouseHoldAccountsItem(string name, string price, DateTime date, string scategory, string dcategory, string storagetype, string isoutogoings)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Date = date.Date.ToString("yyyy/MM/dd");
+            this.SCategory = scategory;
+            this.DCategory = dcategory;
+            this.Storagetype = storagetype;
+            this.IsOutGoings = isoutogoings;
+            this.CategoryData = string.Format("{0}>{1}>{2}",isoutogoings, scategory, dcategory);
+        }
+    }
+}
